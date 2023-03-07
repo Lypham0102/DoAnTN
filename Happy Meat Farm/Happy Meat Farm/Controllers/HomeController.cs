84a8daaac62631f720d3e3d748e1ee37f1e99e47
Lê Thái Khôi Nguyên <97087502+XIVIXMMI@users.noreply.github.com>
2023-03-07 23:05:15 +0700

    update_web_admin

---
+++ Happy Meat Farm/Happy Meat Farm/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Happy_Meat_Farm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Happy_Meat_Farm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult QuanLyCongViec()
+        {
+            return View();
+        }
+        public IActionResult QuanLyBanHang()
+        {
+            return View();
+        }
+        public IActionResult QuanLyVatNuoi()
+        {
+            return View();
+        }
+        public IActionResult ThongKe()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Happy Meat Farm/Happy Meat Farm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Happy_Meat_Farm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
