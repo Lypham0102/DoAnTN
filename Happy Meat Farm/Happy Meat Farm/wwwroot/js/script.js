faec799a7acc8c67f1752fa7b1201ea526d4551c
Phạm Công Lý <lyphamvjit1201@gmail.com>
2023-03-14 00:35:53 +0700

    load_page

---
+++ Happy Meat Farm/Happy Meat Farm/wwwroot/js/script.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+    }, 1500);
+
+    angular
+        .module('app')
+        .directive('script', script);
+
+    script.$inject = ['$window'];
+
+})();
